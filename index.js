003125688baecbef06f243276d482a0552fc0d09
Siegwald Thomas <contact@siegwaldthomas.com>
2025-05-12 21:16:46 +0200

    push

--- index.js
+++ index.js
@@ -7,27 +7,31 @@ import authRoutes from "./routes/auth.routes.js";
 
 // Charger les variables d'env
 dotenv.config();
-
-// Connexion à MongoDB
 connectDB();
 
-// Initialiser express
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://empreinte-app.fr"],
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://empreinte-app.fr",
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
 
-// Middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// Routes
 app.use("/api", auditRoutes);
 app.use("/api/auth", authRoutes);
 
-// Lancement du serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
