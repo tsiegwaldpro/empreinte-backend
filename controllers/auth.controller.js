3a380c46fbab15670464f3cd95d27d1d0605b126
Siegwald Thomas <contact@siegwaldthomas.com>
2025-05-13 17:28:49 +0200

    discord notif

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import { sendEmail } from "../utils/sendEmail.js";
 
-const baseUrl = process.env.baseURL || "http://www.empreinte-app.fr";
+const baseUrl = process.env.URL || "https://www.empreinte-app.fr/";
 
 const JWT_SECRET = process.env.JWT_SECRET || "super_secret_key";
 
